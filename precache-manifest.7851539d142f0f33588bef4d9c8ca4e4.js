a15574f2709036054114c41acbb9f40b26062e19
kevin <boogerlad@gmail.com>
2020-10-31 19:08:51 -0400

    Updates

---
+++ precache-manifest.7851539d142f0f33588bef4d9c8ca4e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e10025e7900f2143bcc0fdc16999d3e",
+    "url": "/tic-tac-toe-solid/index.html"
+  },
+  {
+    "revision": "b1b72c5c45e1491af2bc",
+    "url": "/tic-tac-toe-solid/static/css/main.23f05770.chunk.css"
+  },
+  {
+    "revision": "0fe5402f0c11f7d96b31",
+    "url": "/tic-tac-toe-solid/static/js/2.f4d2ba0c.chunk.js"
+  },
+  {
+    "revision": "b1b72c5c45e1491af2bc",
+    "url": "/tic-tac-toe-solid/static/js/main.07e658cf.chunk.js"
+  },
+  {
+    "revision": "794d0e9653169994df1e",
+    "url": "/tic-tac-toe-solid/static/js/runtime~main.309be72a.js"
+  }
+]);
